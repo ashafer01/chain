e7ea9b3fb2b92459c581dfc0ebc424300cd333c0
Alex Shafer <ashafer@pm.me>
2019-08-18 14:37:19 -0600

    pypi ready

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='function-chain',
+    version='0.1.0',
+    description='Python function chaining',
+    long_description='Python function chaining in a minimalist module',
+    author='Alex Shafer',
+    author_email='ashafer@pm.me',
+    url='https://github.com/ashafer01/chain',
+    license='MIT',
+    py_modules=['chain'],
+)
